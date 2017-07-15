09c45d529fae270c420c2b1f4b24e6dcc8e06e59
hyan <hyan@lambdacal.com>
2017-07-15 22:55:37 +0800

    rename

--- docker_build.sh
+++ docker_build.sh
@@ -1,1 +1,1 @@
-docker build -t visualizer_example:pv5.4.0-v2.2.2 .
+docker build -t hyangit/visualizer_example:pv5.4.0-v2.2.2 .
--- docker_run.sh
+++ docker_run.sh
@@ -1,1 +1,1 @@
-docker run -it --rm -p 8080:8080 visualizer_example:pv5.4.0-v2.2.2
+docker run -it --rm -p 8080:8080 hyangit/visualizer_example:pv5.4.0-v2.2.2
